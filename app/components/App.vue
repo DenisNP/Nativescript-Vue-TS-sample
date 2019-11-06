3be3cb4beff8392ab97c6a5e89a86528d60394a5
Denis Peshekhonov <denis@peshekhonov.ru>
2019-11-06 17:37:18 +0300

    initial

---
+++ app/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <Label class="message" :text="msg" col="0" row="0"/>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from "vue-property-decorator";
+    @Component
+    export default class App extends Vue {
+        msg: string = "Hello";
+    }
+    // export default {
+    //     data() {
+    //         return {
+    //             msg: 'Hello World!'
+    //         }
+    //     }
+    // }
+</script>
+
+<style scoped>
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #000;
+    }
+</style>
